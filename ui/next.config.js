98e705eb6b93d2d36c3b560d8d68237019a0eef1
Janith Rathnayaka <janithr@xeptaon.com>
2024-11-21 13:42:35 +0530

    change it staging

--- ui/next.config.js
+++ ui/next.config.js
@@ -9,7 +9,7 @@ const nextConfig = {
   assetPrefix: "/undp-digital-development-compass",
   env: {
     PASSWORD_PROTECT: process.env.NODE_ENV !== "development",
-    SITE_CONFIG: process.env.ENV ?? "dev"
+    SITE_CONFIG: process.env.ENV ?? "staging"
   },
   // i18n: {
   //   locales: ["en"],
